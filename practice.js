52041db1c35f191820ccae79af71fbdd71d6ff2d
username <Ryan>
2023-01-27 12:55:05 -0600

    second push

--- practice.js
+++ practice.js
@@ -77,14 +77,58 @@
 
 // https://jsonplaceholder.typicode.com/todos
 
-async function filterTodo() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const data = await response.json();
+// async function filterTodo() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+//   const data = await response.json();
 
-  const incompleteTodo = data
-    .filter((element) => !element.completed)
-    .slice(0, 6);
-  console.log(incompleteTodo);
-}
+//   const incompleteTodo = data
+//     .filter((element) => !element.completed)
+//     .slice(0, 6);
+//   console.log(incompleteTodo);
+// }
 
-filterTodo();
+// filterTodo();
+
+// Setup
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//   },
+//   {
+//     firstName: "Sherlock",
+//     lastName: "Holmes",
+//     number: "0487345643",
+//     likes: ["Intriguing Cases", "Violin"],
+//   },
+//   {
+//     firstName: "Kristian",
+//     lastName: "Vos",
+//     number: "unknown",
+//     likes: ["JavaScript", "Gaming", "Foxes"],
+//   },
+// ];
+
+// function lookUpProfile(name, prop) {
+//   // Only change code below this line
+
+//   for (let i = 0; i < contacts.length; i++) {
+//     if (contacts[i].firstName === name || contacts[i].lastName === name) {
+//       return contacts[i][prop] || "No such property";
+//     }
+//   }
+//   return "No such Contact";
+//   // Only change code above this line
+// }
+
+// const data = lookUpProfile("Vos", "number");
+
+// console.log(data);
